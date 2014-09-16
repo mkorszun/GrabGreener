acc41ec76d2f34a5b093a2e66bdce9fa2b59f201
Mateusz Korszun <mkorszun@gmail.com>
2014-09-16 23:09:15 +0200

    Add thank you dialog

--- app/src/main/java/com/greenassitant/dialogs/TreeLimitDialog.java
+++ app/src/main/java/com/greenassitant/dialogs/TreeLimitDialog.java
@@ -14,21 +14,39 @@ public class TreeLimitDialog {
     public void show(Context context, double treeUsage) {
         if (treeUsage > 1 && treeUsage < 5 && !isOneShown()) {
             show(context, context.getResources().getString(R.string.one_tree_usage_msg),
-                    context.getResources().getString(R.string.one_tree_dialog_confirmation));
+                    context.getResources().getString(R.string.one_tree_dialog_confirmation), false);
             setOneShown(true);
         }
 
         if (treeUsage >= 5 && !isFiveShown()) {
             show(context, context.getResources().getString(R.string.five_tree_usage_msg),
-                    context.getResources().getString(R.string.five_tree_dialog_confirmation));
+                    context.getResources().getString(R.string.five_tree_dialog_confirmation), true);
             setFiveShown(true);
         }
     }
 
-    public void show(Context context, String message, String confirmation) {
+    public void show(final Context context, String message, String confirmation, final boolean thankYou) {
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
         builder.setMessage(message)
                 .setPositiveButton(confirmation, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        if (thankYou) {
+                            showThankYou(context);
+                        }
+                    }
+                });
+        builder.create().show();
+    }
+
+    public void showThankYou(Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setMessage(context.getResources().getString(R.string.thank_you))
+                .setPositiveButton("TWITTER", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // TODO
+                    }
+                })
+                .setNegativeButton("FACEBOOK", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         // TODO
                     }
